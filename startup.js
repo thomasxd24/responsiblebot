08d1378ff56f4525f19735c03bb4b4328dd68144
Thomas <thomasxd24@hotmail.com>
2017-09-26 22:26:40 +0200

    change limit of anti-spam

--- startup.js
+++ startup.js
@@ -12,9 +12,10 @@ fs.readdir("./commands/", (err, files) => {
   if (err) return console.error(err);
   let jsfiles = files.filter(f => f.split(".").pop()=="js");
   if(jsfiles.length <= 0) {
-    console.log("No commands to lad!");
+    console.log("No commands to load!");
     return;
   }
+
   console.log(`Loading ${jsfiles.length} commands.`)
   jsfiles.forEach((f, i) => {
     let props = require(`./commands/${f}`);
@@ -36,12 +37,12 @@ bot.on('ready',()=>{
   var anti_spam = require("./anti-spam.js");
   anti_spam(bot, {
     warnBuffer: 3, //Maximum amount of messages allowed to send in the interval time before getting warned.
-    maxBuffer: 10, // Maximum amount of messages allowed to send in the interval time before getting banned.
-    interval: 1000, // Amount of time in ms users can send a maximum of the maxBuffer variable before getting banned.
-    warningMessage: "stop spamming or I'll whack your head off.", // Warning message send to the user indicating they are going to fast.
+    maxBuffer: 5, // Maximum amount of messages allowed to send in the interval time before getting banned.
+    interval: 4000, // Amount of time in ms users can send a maximum of the maxBuffer variable before getting banned.
+    warningMessage: "stop spamming or you will be warned/muted.", // Warning message send to the user indicating they are going to fast.
     banMessage: "has been muted for spamming for 5 minutes, anyone else?", // Ban message, always tags the banned user in front of it.
     maxDuplicatesWarning : 3, // Maximum amount of duplicate messages a user can send in a timespan before getting warned
-    maxDuplicatesBan : 10, // Maximum amount of duplicate messages a user can send in a timespan before getting banned
+    maxDuplicatesBan : 5, // Maximum amount of duplicate messages a user can send in a timespan before getting banned
 });
 })
 
