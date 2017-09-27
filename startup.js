0f83e44bd5570cbb68cc1be671f61680339c2f04
Thomas <thomasxd24@hotmail.com>
2017-09-27 23:38:43 +0200

    changed startup script

--- startup.js
+++ startup.js
@@ -1,69 +1,94 @@
-const Discord = require("discord.js");
-const bot = new Discord.Client();
 const fs = require("fs");
 const Enmap = require('enmap');
+const commando = require('discord.js-commando');
+const path = require('path');
+const oneLine = require('common-tags').oneLine;
+const sqlite = require('sqlite');
 const config = require("./config.json");
-const prefix = config.prefix;
-bot.commands = new Discord.Collection();
 
+const client = new commando.Client({
+	owner: '186824408227119104',
+	commandPrefix: '/'
+});
 
-// This loop reads the /events/ folder and attaches each event file to the appropriate event.
-fs.readdir("./commands/", (err, files) => {
-  if (err) return console.error(err);
-  let jsfiles = files.filter(f => f.split(".").pop()=="js");
-  if(jsfiles.length <= 0) {
-    console.log("No commands to load!");
-    return;
-  }
-
-  console.log(`Loading ${jsfiles.length} commands.`)
-  jsfiles.forEach((f, i) => {
-    let props = require(`./commands/${f}`);
-    console.log(`${i+1} : ${f} Loaded`);
-    bot.commands.set(props.help.name, props);
-
+client
+	.on('error', console.error)
+	.on('warn', console.warn)
+	.on('debug', console.log)
+	.on('ready', () => {
+    client.relationRequest = new Enmap({name: "relationRequest",persistent: true});
+    client.relation = new Enmap({name: "relation",persistent: true});
+    client.muted = new Enmap({name: "muted",persistent: true});
+    client.offences = new Enmap({name: "offences",persistent: true});
+    console.log("Loaded relationRequest");
+    console.log("Loaded relation");
+    console.log("Loaded muted");
+    console.log("Loaded offences");
+    client.user.setGame(`with BigBrother.`);
+    var anti_spam = require("./anti-spam.js");
+    anti_spam(client, {
+      warnBuffer: 3, //Maximum amount of messages allowed to send in the interval time before getting warned.
+      maxBuffer: 5, // Maximum amount of messages allowed to send in the interval time before getting banned.
+      interval: 4000, // Amount of time in ms users can send a maximum of the maxBuffer variable before getting banned.
+      warningMessage: "stop spamming or you will be warned/muted.", // Warning message send to the user indicating they are going to fast.
+      banMessage: "has been muted for spamming for 5 minutes, anyone else?", // Ban message, always tags the banned user in front of it.
+      maxDuplicatesWarning : 3, // Maximum amount of duplicate messages a user can send in a timespan before getting warned
+      maxDuplicatesBan : 5, // Maximum amount of duplicate messages a user can send in a timespan before getting banned
   });
-});
+    console.log(`Client ready; logged in as ${client.user.username}#${client.user.discriminator} (${client.user.id})`);
+	})
+	.on('disconnect', () => { console.warn('Disconnected!'); })
+	.on('reconnecting', () => { console.warn('Reconnecting...'); })
+	.on('commandError', (cmd, err) => {
+		if(err instanceof commando.FriendlyError) return;
+		console.error(`Error in command ${cmd.groupID}:${cmd.memberName}`, err);
+	})
+	.on('commandBlocked', (msg, reason) => {
+		console.log(oneLine`
+			Command ${msg.command ? `${msg.command.groupID}:${msg.command.memberName}` : ''}
+			blocked; ${reason}
+		`);
+	})
+	.on('commandPrefixChange', (guild, prefix) => {
+		console.log(oneLine`
+			Prefix ${prefix === '' ? 'removed' : `changed to ${prefix || 'the default'}`}
+			${guild ? `in guild ${guild.name} (${guild.id})` : 'globally'}.
+		`);
+	})
+	.on('commandStatusChange', (guild, command, enabled) => {
+		console.log(oneLine`
+			Command ${command.groupID}:${command.memberName}
+			${enabled ? 'enabled' : 'disabled'}
+			${guild ? `in guild ${guild.name} (${guild.id})` : 'globally'}.
+		`);
+	})
+	.on('groupStatusChange', (guild, group, enabled) => {
+		console.log(oneLine`
+			Group ${group.id}
+			${enabled ? 'enabled' : 'disabled'}
+			${guild ? `in guild ${guild.name} (${guild.id})` : 'globally'}.
+		`);
+	})
+	.on("message", async message => {
+	  if(message.member.roles.some(r=>["Muted"].includes(r.name)) )
+	     return message.delete().catch(O_o=>{});
+	});
 
+client.setProvider(
+	sqlite.open(path.join(__dirname, 'database.sqlite3')).then(db => new commando.SQLiteProvider(db))
+).catch(console.error);
 
-bot.on('ready',()=>{
-  console.log('Bot is ready!');
-  bot.relationRequest = new Enmap({name: "relationRequest",persistent: true});
-  bot.relation = new Enmap({name: "relation",persistent: true});
-  bot.muted = new Enmap({name: "muted",persistent: true});
-  bot.offences = new Enmap({name: "offences",persistent: true});
-  console.log("Loaded relationRequest");
-  console.log("Loaded relation");
-  console.log("Loaded muted");
-  console.log("Loaded offences");
-  bot.user.setGame(`with BigBrother.`);
-  var anti_spam = require("./anti-spam.js");
-  anti_spam(bot, {
-    warnBuffer: 3, //Maximum amount of messages allowed to send in the interval time before getting warned.
-    maxBuffer: 5, // Maximum amount of messages allowed to send in the interval time before getting banned.
-    interval: 4000, // Amount of time in ms users can send a maximum of the maxBuffer variable before getting banned.
-    warningMessage: "stop spamming or you will be warned/muted.", // Warning message send to the user indicating they are going to fast.
-    banMessage: "has been muted for spamming for 5 minutes, anyone else?", // Ban message, always tags the banned user in front of it.
-    maxDuplicatesWarning : 3, // Maximum amount of duplicate messages a user can send in a timespan before getting warned
-    maxDuplicatesBan : 5, // Maximum amount of duplicate messages a user can send in a timespan before getting banned
-});
-})
+client.registry
+.registerDefaultTypes()
+  .registerGroups([
+      ['moderation', 'Moderation'],
+      ['rp', 'RolePlay'],
+      ['util', 'Utility'],
+      ['misc','Miscellaneous'],
 
-bot.on("message", async message => {
-  if(message.author.bot) return;
-  if(message.channel.type === "dm") message.channel.send("LOL");
-  if(message.member.roles.some(r=>["Muted"].includes(r.name)) )
-     return message.delete().catch(O_o=>{});
-  let messageArrary = message.content.split(/\s+/g);
-  let command = messageArrary[0];
-  let args = messageArrary.slice(1);
-  if(!command.startsWith(prefix))  return;
-  let cmd = bot.commands.get(command.slice(prefix.length));
-  if(cmd) cmd.run(bot, message ,args);
+  ])
+  .registerDefaultGroups()
+  .registerDefaultCommands()
+  .registerCommandsIn(path.join(__dirname, 'commands'));
 
-
-});
-
-
-
-bot.login(config.token);
+client.login(config.token);
