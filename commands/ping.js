4deaa7e4e703fe859d1b6167bcd05fc91782f980
Thomas <thomasxd24@hotmail.com>
2017-09-27 21:53:09 +0200

    deleted node_modules in rep, changed command system to commando, added folder for commandGroup

--- commands/ping.js
+++
@@ -1,8 +0,0 @@
-exports.run = async (client, message, [mention, ...reason]) => {
-  const m = await message.channel.send("Ping?");
-  m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
-
-};
-exports.help = {
-  name:"ping",
-}
