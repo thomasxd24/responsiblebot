779a65adc80adce66c1087fb4ba352b8e525b7a3
Thomas Tse <thomasxd24@hotmail.com>
2017-11-21 20:04:28 +0100

    added ship

---
+++ commands/misc/ship.js
@@ -0,0 +1,33 @@
+const { Command } = require('discord.js-commando');
+const permissionRole = "BeginningGamer";
+module.exports = class ShipCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'ship',
+            group: 'misc',
+            memberName: 'ship',
+            description: 'Ship someone.',
+            examples: ['/ship']
+        });
+    }
+
+    hasPermission(msg) {
+      const userMaxPermission = msg.member.roles.sort((r1, r2) => r2.calculatedPosition - r1.calculatedPosition).first().calculatedPosition;
+      if(msg.guild.roles.find("name",permissionRole) == null)
+      {
+        return false;
+      }
+      const cmdPermission = msg.guild.roles.find("name",permissionRole).calculatedPosition;
+      if(userMaxPermission >= cmdPermission)
+      {
+        return true;
+      }
+      else {
+        return false;
+      }
+    }
+
+    run(msg) {
+      msg.channel.send("I ship "+message.guild.members.random().user.username+" and "+message.guild.members.random().user.username)
+    }
+};
